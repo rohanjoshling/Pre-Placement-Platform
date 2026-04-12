3a987b2dd4a1b2f491794cf4f1b753e14692d5c7
Sarthak <thapliyalsarthak38@gmail.com>
2026-04-12 14:35:15 +0530

    added profile changes

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from models import user
 from database.database import get_db
 from core.security import get_password_hash, verify_password
 from CRUD import user_crud
@@ -35,3 +36,42 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
             "email": db_user.email
         }
     }
+
+@router.put("/update-profile")
+def update_profile(email: str, name: str, db: Session = Depends(get_db)):
+    user = user_crud.get_user_by_email(db, email=email)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user.name = name
+    db.commit()
+
+    return {"message": "Name updated successfully"}
+
+@router.put("/change-password")
+def change_password(email: str, old_password: str, new_password: str, db: Session = Depends(get_db)):
+    user = user_crud.get_user_by_email(db, email=email)
+    
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    if not verify_password(old_password, user.password):
+        raise HTTPException(status_code=400, detail="Old password is incorrect")
+    
+    user.password = get_password_hash(new_password)
+    db.commit()
+
+    return  {"message": "Password updated successfully"}
+
+@router.delete("/delete-account")
+def delete_account(email: str, db: Session = Depends(get_db)):
+    user = user_crud.get_user_by_email(db, email=email)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(user)
+    db.commit()
+
+    return {"message": "Account deleted successfully"}
