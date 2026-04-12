78d7829645c039bebc6803a84146ab10a6bbc199
Sarthak <thapliyalsarthak38@gmail.com>
2026-04-12 15:50:13 +0530

    use of jwt handler to change password , profiles names

--- backend/core/jwt_handler.py
+++ backend/core/jwt_handler.py
@@ -1,4 +1,4 @@
-from jose import jwt
+from jose import jwt, JWTError
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
@@ -14,3 +14,10 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload  
+    except JWTError:
+        return None
--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,13 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from models import user
+from fastapi.security import OAuth2PasswordBearer
 from database.database import get_db
 from core.security import get_password_hash, verify_password
 from CRUD import user_crud
 from schemas.user_schema import UserBase , UserResponse , UserLogin
-from core.jwt_handler import create_access_token
+from core.jwt_handler import create_access_token , decode_access_token
 
 router = APIRouter(prefix= "/auth", tags=["auth"])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user_email(token: str = Depends(oauth2_scheme)):
+    payload = decode_access_token(token)
+
+    if payload is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    return payload.get("sub")
 
 @router.post("/register", response_model=UserResponse)
 def register(user: UserBase, db: Session = Depends(get_db)):
@@ -38,38 +48,34 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
     }
 
 @router.put("/update-profile")
-def update_profile(email: str, name: str, db: Session = Depends(get_db)):
+def update_profile(name: str, db: Session = Depends(get_db), email: str = Depends(get_current_user_email)):
     user = user_crud.get_user_by_email(db, email=email)
-
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
 
     user.name = name
     db.commit()
 
-    return {"message": "Name updated successfully"}
+    return {"message": "Name updated"}
 
 @router.put("/change-password")
-def change_password(email: str, old_password: str, new_password: str, db: Session = Depends(get_db)):
+def change_password(old_password: str, new_password: str,
+                    db: Session = Depends(get_db),
+                    email: str = Depends(get_current_user_email)):
+
     user = user_crud.get_user_by_email(db, email=email)
-    
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    
+
     if not verify_password(old_password, user.password):
         raise HTTPException(status_code=400, detail="Old password is incorrect")
-    
+
     user.password = get_password_hash(new_password)
     db.commit()
 
-    return  {"message": "Password updated successfully"}
+    return {"message": "Password updated successfully"}
 
 @router.delete("/delete-account")
-def delete_account(email: str, db: Session = Depends(get_db)):
-    user = user_crud.get_user_by_email(db, email=email)
+def delete_account(db: Session = Depends(get_db),
+                   email: str = Depends(get_current_user_email)):
 
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+    user = user_crud.get_user_by_email(db, email=email)
 
     db.delete(user)
     db.commit()
